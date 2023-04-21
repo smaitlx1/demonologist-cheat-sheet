866070e78bf44188af46a5fa14871ae2ead84771
Aaron Page <aaron.page@visionnet.us>
2023-04-20 20:17:37 -0500

    Update ghost_template.js

--- scripts-v2/ghost_template.js
+++ scripts-v2/ghost_template.js
@@ -32,15 +32,23 @@ class Ghost {
             }
         }
 
+        if (data.hunt_sanity === "???") {
+            var huntValue = `<img src="imgs/speed/unknown.png" />`
+        } else {
+            var huntValue = data.hunt_sanity;
+        }
+
+
+
         this.ghostTemplate = `
         <div class="ghost_card" id="${data.ghost}">
                 <div class="ghost_name">${data.ghost}</div>
                 <div class="ghost_hunt_info">
-                    <div class="ghost_hunt ${parseInt(data.hunt_sanity) > 50 ?'high':parseInt(data.hunt_sanity) < 50 ? 'low':'average'}"><img src="imgs/sanity.png">${data.hunt_sanity}</div>
-                    <div class="ghost_speed">
+                    <div class="ghost_hunt ${parseInt(data.hunt_sanity) > 50 ?'high':parseInt(data.hunt_sanity) < 50 ? 'low':'average'}"><img src="imgs/sanity.png">${huntValue}</div>
+					<div class="ghost_speed">
                         <img src="imgs/footsteps.png">
                         <div class="ghost_speed_values">
-                            ${this.toNumStr(data.min_speed)} <span class="ms">m/s</span> <span class="sound" onclick="toggleSound(${data.min_speed},'${data.ghost}0')">&#128266;</span>${data.max_speed == null ? '' : data.speed_is_range?' - ':' | '}${data.max_speed == null ? '' : this.toNumStr(data.max_speed)+' <span class="ms">m/s</span> <span class="sound" onclick="toggleSound('+data.max_speed+',\''+data.ghost+'1\')">&#128266;</span>'}${data.alt_speed == null ? '' : '<br>('+this.toNumStr(data.alt_speed)+' <span class="ms">m/s</span> <span class="sound" onclick="toggleSound('+data.alt_speed+',\''+data.ghost+'2\')">&#128266;</span>)'}
+                            <img src=\"imgs/speed/${data.speed}.png\" class=\"speedIcon\" />
                         </div>
                     </div>
                 </div>
@@ -49,7 +57,6 @@ class Ghost {
                     <div class="ghost_evidence_item" ${data.evidence[1] in evi_color ? 'style=\"color:' + evi_color[data.evidence[1]] + ' !important;\"' : ''}><img src="${evi_icons[data.evidence[1]]}">${data.evidence[1]}</div>
                     <div class="ghost_evidence_item" ${data.evidence[2] in evi_color ? 'style=\"color:' + evi_color[data.evidence[2]] + ' !important;\"' : ''}><img src="${evi_icons[data.evidence[2]]}">${data.evidence[2]}</div>
                 </div>
-                <!--<div class="ghost_nightmare_evidence">${data.nightmare_evidence?data.nightmare_evidence:''}</div>-->
                 <div class="ghost_behavior">
                    ${data.behavior.map(this.behavior).join('<hr>')}
                 </div>
@@ -66,11 +73,4 @@ class Ghost {
         return '<div class="ghost_behavior_item">' + value + '</div>'
     }
 
-    toNumStr(num) { 
-        if (Number.isInteger(num)) { 
-          return num + ".0"
-        } else {
-          return num.toString(); 
-        }
-      }
 }
